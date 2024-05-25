b795b0e13622e19b27b1929a7618419420794032
Piyush Chaudhari <piyushchaudhari112@gmail.com>
2024-05-25 17:08:09 +0530

    New File

--- Java Programs/arrayissortedornot.java
+++ Java Programs/arrayissortedornot.java
@@ -1,64 +1,31 @@
-// import java.util.Scanner;
-
-// public class arrayissortedornot {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-
-//         int n= sc.nextInt();
-//         int []a = new int[n];
-//         for(int i=0; i<n; i++){
-//             a[i] = sc.nextInt();
-//         }
-
-//         boolean ans = isSorted(a,n);
-//         if (ans==true){
-//             System.out.println("yes");
-//         }
-//             else{
-//                 System.out.println("No");
-//             }
-//     }
-        
-//             public static boolean isSorted(int a[],int n){
-//                 for(int i =1; i<n; i++){
-//                     if(a[i]<a[i-1]){
-//                         return false;
-//                     }
-                    
-//                 }
-//                 return true;
-//             }
-//         }
-    
 import java.util.Scanner;
 
 public class arrayissortedornot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
+        int n= sc.nextInt();
+        int []a = new int[n];
+        for(int i=0; i<n; i++){
             a[i] = sc.nextInt();
         }
 
-        boolean ans = isSorted(a, n);
-        if (ans == true) {
+        boolean ans = isSorted(a,n);
+        if (ans==true){
             System.out.println("yes");
-        } else {
-            System.out.println("No");
         }
+            else{
+                System.out.println("No");
+            }
     }
-
-    public static boolean isSorted(int[] a, int n) {
-        for (int i = 1; i < n; i++) {
-            if (a[i] < a[i - 1]) {
-                return false;
+        
+            public static boolean isSorted(int a[],int n){
+                for(int i =1; i<n; i++){
+                    if(a[i]<a[i-1]){
+                        return false;
+                    }
+                    
+                }
+                return true;
             }
         }
-        if (n > 1 && a[n - 1] < a[n - 2]) {
-            return false;
-        }
-        return true;
-    }
-}
